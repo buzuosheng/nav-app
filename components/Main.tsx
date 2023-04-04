3af7610f89973d7bcc20dcb85059d64c2ba675e7
buzuosheng <1015350043@qq.com>
2023-04-05 03:42:09 +0800

    鼠标点击侧边导航栏样式

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,24 +1,32 @@
 import Card from './Card'
 import { useState } from 'react'
 import { data } from '../lib/sites'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Main = () => {
-  const [id, setId] = useState(0)
+  const [target, setTarget] = useState('rep')
+  console.log(target)
   return (
     <main className="p-10 min-h-screen flex flex-row justify-center items-start bg-[#f2f4f8]">
       <ul className="w-[133px] h-full mr-4 my-0 top-20 left-4 sticky list-none p-0">
         {data.map((cate) => (
           <li key={cate.cate}>
             <div className="flex flex-row items-center">
-              {/* <div
+              <div
                 className={
-                  'w-3 mr-2 border-b-2 border-solid border-zinc-700 ' 
-                  + 'opacity-0 pointer-events-none'
+                  'w-3 mr-2 border-b-2 border-solid border-zinc-700 ' +
+                  'opacity-0 pointer-events-none'
                 }
-              ></div> */}
+              ></div>
               <a className="text-black no-underline" href={`#${cate.title}`}>
-                <div className="p-1 w-auto font-normal rounded hover:text-white hover:bg-[#ff5a00]">
+                <div
+                  className={`p-1 w-auto font-normal rounded hover:text-white hover:bg-[#ff5a00] ${
+                    target === cate.title ? 'text-[#ff5a00]' : ''
+                  }`}
+                  onClick={() => {
+                    setTarget(cate.title)
+                  }}
+                >
                   {cate.cate}
                 </div>
               </a>
