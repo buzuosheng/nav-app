82aec61b66cf0c2884c96f1bfa0bf5a50a1a80e6
buzuosheng <1015350043@qq.com>
2023-04-06 01:03:32 +0800

    侧边导航栏样式

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   const [ value, setValue ] = useState('知乎')
   return (
     <header className="pt-[25px] pb-[15px] w-full text-white bg-slate-600 text-center">
-      <div className="w-full h-auto">
+      <div className="w-full h-auto pt-6 pb-4">
         <div className="h-10" onClick={() => {}}>知乎</div>
       </div>
     </header>
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,11 +1,9 @@
 import Card from './Card'
 import { useState } from 'react'
 import { data } from '../lib/sites'
-import { useRouter } from 'next/router'
 
 const Main = () => {
   const [target, setTarget] = useState('rep')
-  console.log(target)
   return (
     <main className="p-10 min-h-screen flex flex-row justify-center items-start bg-[#f2f4f8]">
       <ul className="w-[133px] h-full mr-4 my-0 top-20 left-4 sticky list-none p-0">
@@ -14,15 +12,18 @@ const Main = () => {
             <div className="flex flex-row items-center">
               <div
                 className={
-                  'w-3 mr-2 border-b-2 border-solid border-zinc-700 ' +
-                  'opacity-0 pointer-events-none'
+                  'w-3 mr-2 border-b-2 border-solid border-zinc-700 duration-500 ' +
+                  (target === cate.title
+                    ? ''
+                    : ' opacity-0 pointer-events-none')
                 }
               ></div>
               <a className="text-black no-underline" href={`#${cate.title}`}>
                 <div
-                  className={`p-1 w-auto font-normal rounded hover:text-white hover:bg-[#ff5a00] ${
-                    target === cate.title ? 'text-[#ff5a00]' : ''
-                  }`}
+                  className={
+                    'p-1 w-auto font-normal rounded hover:text-white hover:bg-[#ff5a00]' +
+                    (target === cate.title ? ' text-[#ff5a00] ' : '')
+                  }
                   onClick={() => {
                     setTarget(cate.title)
                   }}
