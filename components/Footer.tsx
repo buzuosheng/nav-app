91c63761755a5b2b5f5f66bc8ed4840869075049
buzuosheng <1015350043@qq.com>
2023-04-16 18:56:01 +0800

    chore: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <footer className="w-full h-20 bg-slate-600 text-white text-center"> footer</footer>
+  return (
+    <footer className="w-full h-10 py-2 bg-sky-500 text-white text-center">
+      created by <a className="hover:cursor-pointer no-underline">shanyue</a>
+    </footer>
+  )
 }
 
 export default Footer
