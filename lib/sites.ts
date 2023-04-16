8226cec09bf9f4752800fe78bcb9a508ba937f3e
buzuosheng <1015350043@qq.com>
2023-04-16 23:12:29 +0800

    chore: add ozon

--- lib/sites.ts
+++ lib/sites.ts
@@ -36,6 +36,18 @@ export const data = [
     ]
   },
   {
+    title: 'operate',
+    cate: '运营工具',
+    sites: [
+      {
+        site: 'ozon跟卖采集',
+        logo: '/logos/my-ozon.png',
+        url: 'https://ozon.xiange.tech',
+        priority: true
+      }
+    ]
+  },
+  {
     title: 'collection',
     cate: '收款平台',
     sites: [
@@ -99,11 +111,24 @@ export const data = [
         url: 'https://gdeposylka.ru/',
         priority: true
       },
-      { site: 'CDEK查询',        logo: '/logos/cdek.ico', url: 'https://www.cdek.ru/ru/tracking' },
-      { site: '俄罗斯邮政',        logo: '/logos/pochta.png', url: 'https://www.pochta.ru/' },
-      { site: 'hecny',        logo: '/logos/hecny.ico', url: 'https://e-com.hecny.ru/cn' },
       {
-        site: 'gbs线上物流查询网站',        logo: '/logos/gbs-broker.ico',
+        site: 'CDEK查询',
+        logo: '/logos/cdek.ico',
+        url: 'https://www.cdek.ru/ru/tracking'
+      },
+      {
+        site: '俄罗斯邮政',
+        logo: '/logos/pochta.png',
+        url: 'https://www.pochta.ru/'
+      },
+      {
+        site: 'hecny',
+        logo: '/logos/hecny.ico',
+        url: 'https://e-com.hecny.ru/cn'
+      },
+      {
+        site: 'gbs线上物流查询网站',
+        logo: '/logos/gbs-broker.ico',
         url: 'https://gbs-broker.ru/en#tracking_search_results'
       }
     ]
@@ -113,13 +138,20 @@ export const data = [
     cate: '辅助工具',
     sites: [
       {
-        site: '图片翻译/转链接',        logo: '/logos/xiangjifanyi.ico',
+        site: '图片翻译/转链接',
+        logo: '/logos/xiangjifanyi.ico',
         url: 'https://www.xiangjifanyi.com/home/',
         priority: true
       },
-      { site: '图片转网址',        logo: '/logos/wampi.ico', url: 'https://wampi.ru', priority: true },
       {
-        site: '图片翻译',        logo: '/logos/translate-yandex.ico',
+        site: '图片转网址',
+        logo: '/logos/wampi.ico',
+        url: 'https://wampi.ru',
+        priority: true
+      },
+      {
+        site: '图片翻译',
+        logo: '/logos/translate-yandex.ico',
         url: 'https://translate.yandex.ru/ocr',
         priority: true
       }
@@ -129,13 +161,24 @@ export const data = [
     title: 'translate',
     cate: '翻译工具',
     sites: [
-      { site: '谷歌翻译',         logo: '/logos/translate-google.ico',url: 'https://translate.google.cn/', priority: true },
       {
-        site: 'Deepl',        logo: '/logos/deepl.svg',
+        site: '谷歌翻译',
+        logo: '/logos/translate-google.ico',
+        url: 'https://translate.google.cn/',
+        priority: true
+      },
+      {
+        site: 'Deepl',
+        logo: '/logos/deepl.svg',
         url: 'https://www.deepl.com/translator',
         priority: true
       },
-      { site: 'Yandex',        logo: '/logos/translate-yandex.ico', url: 'https://translate.yandex.com/', priority: true }
+      {
+        site: 'Yandex',
+        logo: '/logos/translate-yandex.ico',
+        url: 'https://translate.yandex.com/',
+        priority: true
+      }
     ]
   },
   {
@@ -143,7 +186,8 @@ export const data = [
     cate: '浏览器',
     sites: [
       {
-        site: '紫鸟',        logo: '/logos/ziniao.ico',
+        site: '紫鸟',
+        logo: '/logos/ziniao.ico',
         url: 'https://www.ziniao.com/?from=9648&utm_source=9648',
         priority: true
       }
@@ -154,11 +198,17 @@ export const data = [
     cate: '平台网站',
     sites: [
       {
-        site: 'OZON卖家登录网址',        logo: '/logos/ozon-seller.png',
+        site: 'OZON卖家登录网址',
+        logo: '/logos/ozon-seller.png',
         url: 'https://seller.ozon.ru/app/',
         priority: true
       },
-      { site: 'OZON买家登录网址',        logo: '/logos/ozon-buyer.ico', url: 'https://www.ozon.ru/', priority: true }
+      {
+        site: 'OZON买家登录网址',
+        logo: '/logos/ozon-buyer.ico',
+        url: 'https://www.ozon.ru/',
+        priority: true
+      }
     ]
   },
   {
@@ -166,12 +216,14 @@ export const data = [
     cate: '采购网站',
     sites: [
       {
-        site: '1688',        logo: '/logos/1688.ico',
+        site: '1688',
+        logo: '/logos/1688.ico',
         url: 'https://www.1688.com/',
         priority: true
       },
       {
-        site: '义乌购',        logo: '/logos/yiwugou.ico',
+        site: '义乌购',
+        logo: '/logos/yiwugou.ico',
         url: 'https://www.yiwugo.com/',
         priority: true
       },
@@ -182,42 +234,50 @@ export const data = [
         priority: true
       },
       {
-        site: '拼多多',        logo: '/logos/pinduoduo.ico',
+        site: '拼多多',
+        logo: '/logos/pinduoduo.ico',
         url: 'https://www.pinduoduo.com/',
         priority: true
       },
       {
-        site: '京东',        logo: '/logos/jd.ico',
+        site: '京东',
+        logo: '/logos/jd.ico',
         url: 'https://www.jd.com/',
         priority: true
       },
       {
-        site: '环球资源',        logo: '/logos/supplier-globalsources.ico',
+        site: '环球资源',
+        logo: '/logos/supplier-globalsources.ico',
         url: 'https://www.supplier-globalsources.com/'
       },
       {
-        site: '环球华品',        logo: '/logos/saleyee.ico',
+        site: '环球华品',
+        logo: '/logos/saleyee.ico',
         url: 'https://www.saleyee.cn/'
       },
       {
-        site: '53货源网',        logo: '/logos/53shop.ico',
+        site: '53货源网',
+        logo: '/logos/53shop.ico',
         url: 'https://www.53shop.com/'
       },
       {
-        site: '慧聪网',        
+        site: '慧聪网',
         logo: '/logos/hc360.png',
         url: 'https://www.hc360.com/'
       },
       {
-        site: '玩具巴巴',        logo: '/logos/toybaba.ico',
+        site: '玩具巴巴',
+        logo: '/logos/toybaba.ico',
         url: 'https://www.toybaba.com/'
       },
       {
-        site: '生意网',        logo: '/logos/3e3e.ico',
+        site: '生意网',
+        logo: '/logos/3e3e.ico',
         url: 'http://3e3e.go2.cn/'
       },
       {
-        site: '鞋库网',        logo: '/logos/cnxieku.ico',
+        site: '鞋库网',
+        logo: '/logos/cnxieku.ico',
         url: 'https://www.cnxieku.com/'
       }
     ]
@@ -227,62 +287,74 @@ export const data = [
     cate: '聊天AI',
     sites: [
       {
-        site: 'ChatGPT',        logo: '/logos/chatgpt.ico',
+        site: 'ChatGPT',
+        logo: '/logos/chatgpt.ico',
         desc: '地表最强AI聊天机器人',
         url: 'https://chat.openai.com/'
       },
       {
-        site: 'Bard',        logo: '/logos/gdeposylka.ico',
+        site: 'Bard',
+        logo: '/logos/gdeposylka.ico',
         desc: 'Google推出的AI聊天对话机器人Bard',
         url: 'https://bard.google.com/'
       },
       {
-        site: '文心一言',        logo: '/logos/yiyan.ico',
+        site: '文心一言',
+        logo: '/logos/yiyan.ico',
         desc: '百度推出的基于文心大模型的AI对话互动工具',
         url: 'https://yiyan.baidu.com/'
       },
       {
-        site: 'Bing必应',        logo: '/logos/bing.ico',
+        site: 'Bing必应',
+        logo: '/logos/bing.ico',
         desc: '微软推出的新版结合了ChatGPT功能的必应',
         url: 'https://cn.bing.com/'
       },
       {
-        site: 'Anthropic',        logo: '/logos/anthropic.ico',
+        site: 'Anthropic',
+        logo: '/logos/anthropic.ico',
         desc: 'Anthropic发布的与ChatGPT竞争的聊天机器人',
         url: 'https://www.anthropic.com/'
       },
       {
-        site: 'Perplexity',        logo: '/logos/perplexity.ico',
+        site: 'Perplexity',
+        logo: '/logos/perplexity.ico',
         desc: '智能总结并展示信息源',
         url: 'https://www.perplexity.ai/'
       },
       {
-        site: 'Jasper Chat',        logo: '/logos/jasper.svg',
+        site: 'Jasper Chat',
+        logo: '/logos/jasper.svg',
         desc: 'Jasper针对内容创作者出品的AI聊天工具',
         url: 'https://www.jasper.ai/chat'
       },
       {
-        site: 'Poe',        logo: '/logos/poe.ico',
+        site: 'Poe',
+        logo: '/logos/poe.ico',
         desc: '问答社区Quora推出的问答机器人工具',
         url: 'https://poe.com/'
       },
       {
-        site: 'Character.AI',        logo: '/logos/character.ico',
+        site: 'Character.AI',
+        logo: '/logos/character.ico',
         desc: '创建虚拟角色并与其对话',
         url: 'https://character.ai/'
       },
       {
-        site: 'YouChat',        logo: '/logos/you.webp',
+        site: 'YouChat',
+        logo: '/logos/you.webp',
         desc: 'AI搜索对话工具',
         url: 'https://you.com/chat'
       },
       {
-        site: 'ChatSonic',        logo: '/logos/writesonic.png',
+        site: 'ChatSonic',
+        logo: '/logos/writesonic.png',
         desc: 'WriteSonic出品的ChatGPT竞品',
         url: 'https://writesonic.com/chat'
       },
       {
-        site: 'IngestAI',        logo: '/logos/ingestai.png',
+        site: 'IngestAI',
+        logo: '/logos/ingestai.png',
         desc: '一种帮助人们将他们的知识库转变为类似聊天机器人的助手的工具。该工具与Slack、Disc',
         url: 'https://ingestai.io/'
       }
@@ -293,83 +365,98 @@ export const data = [
     cate: '绘画AI',
     sites: [
       {
-        site: 'Midjourney',        logo: '/logos/midjourney.ico',
+        site: 'Midjourney',
+        logo: '/logos/midjourney.ico',
         desc: '目前最强的AI绘画工具',
         url: 'https://www.midjourney.com/home/?callbackUrl=%2Fapp%2F'
       },
       {
-        site: 'Stable Diffusion',        logo: '/logos/stability.png',
+        site: 'Stable Diffusion',
+        logo: '/logos/stability.png',
         desc: '最强开源AI绘画工具',
         url: 'https://stability.ai/'
       },
       {
-        site: '文心一格',        logo: '/logos/yige.ico',
+        site: '文心一格',
+        logo: '/logos/yige.ico',
         desc: '百度出品的AI绘画工具',
         url: 'https://yige.baidu.com/'
       },
       {
-        site: 'Civitai',        logo: '/logos/civitai.ico',
+        site: 'Civitai',
+        logo: '/logos/civitai.ico',
         desc: 'AI艺术共享平台！海量SD开源模型',
         url: 'https://civitai.com/'
       },
       {
-        site: 'NijiJourney',        logo: '/logos/nijijourney.ico',
+        site: 'NijiJourney',
+        logo: '/logos/nijijourney.ico',
         desc: 'MJ出品！面向二次元风格，内容细致拿捏专业到位',
         url: 'https://nijijourney.com/zh/'
       },
       {
-        site: 'Dreamlike.art',        logo: '/logos/dreamlike.webp',
+        site: 'Dreamlike.art',
+        logo: '/logos/dreamlike.webp',
         desc: '效果惊人！内置5种模型的AI图像生成器',
         url: 'https://dreamlike.art/'
       },
       {
-        site: 'HuggingFace',        logo: '/logos/huggingface.ico',
+        site: 'HuggingFace',
+        logo: '/logos/huggingface.ico',
         desc: '下载开源的SD模型',
         url: 'https://huggingface.co/'
       },
       {
-        site: 'DreamUp',      
-          logo: '/logos/dreamup.ico',
+        site: 'DreamUp',
+        logo: '/logos/dreamup.ico',
         desc: 'DeviantArt推出的AI插画生成工具',
         url: 'https://www.dreamup.com/'
       },
       {
-        site: 'Lexica',        logo: '/logos/lexica.ico',
+        site: 'Lexica',
+        logo: '/logos/lexica.ico',
         desc: '基于StableDiffusion的在线插画生成',
         url: 'https://lexica.art/'
       },
       {
-        site: 'Scribble Diffusion',        logo: '/logos/scribblediffusion.ico',
+        site: 'Scribble Diffusion',
+        logo: '/logos/scribblediffusion.ico',
         desc: '将草图转变为精美的插画',
         url: 'https://scribblediffusion.com/'
       },
       {
-        site: 'Artbreeder',        logo: '/logos/artbreeder.png',
+        site: 'Artbreeder',
+        logo: '/logos/artbreeder.png',
         desc: '创建令人惊叹的插画和艺术',
         url: 'https://www.artbreeder.com/'
       },
       {
-        site: 'Leonardo',        logo: '/logos/leonardo.webp',
+        site: 'Leonardo',
+        logo: '/logos/leonardo.webp',
         desc: 'AI绘图社区！训练自己的游戏资产模型',
         url: 'https://leonardo.ai/'
       },
       {
-        site: 'AISEO ART',        logo: '/logos/aiseo-symble.png',
+        site: 'AISEO ART',
+        logo: '/logos/aiseo-symble.png',
         desc: 'AISEO Art 的AI算法按照分类进行过细致的训练',
         url: 'https://art.aiseo.ai/'
       },
       {
-        site: 'DreamStudio',        logo: '/logos/dreamstudio.svg',
+        site: 'DreamStudio',
+        logo: '/logos/dreamstudio.svg',
         desc: 'SD兄弟产品！AI 图像生成器',
         url: 'https://beta.dreamstudio.ai/'
       },
       {
-        site: 'NightCafeAI',        logo: '/logos/nightcafe.png',
+        site: 'NightCafeAI',
+        logo: '/logos/nightcafe.png',
         desc: '艺术插画在线生成',
         url: 'https://creator.nightcafe.studio/'
       },
       {
-        site: '无界AI',        logo: '/logos/wujieai.ico',
+        site: '无界AI',
+        logo: '/logos/wujieai.ico',
         desc: '提供一站式AI搜索创作交流分享服务',
         url: 'https://www.wujieai.com/'
       }
